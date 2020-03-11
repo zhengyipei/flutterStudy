60e81cac3b7c4561e31777da2db2667c03aecaa8
zhengyipei <zhengyipei@126.com>
2020-03-11 18:45:35 +0800

    no message

--- flutter_monkey/lib/util/log_utils.dart
+++ flutter_monkey/lib/util/log_utils.dart
@@ -35,4 +35,16 @@ class Log {
     }
   }
   
+  static void _printMap(Map data, {tag:tag,int tabs = 1, bool isListItem = false, bool isLast = false}){
+    final bool isRoot = tabs == 1;
+    final initialIndent = _indent(tabs);
+    tabs++;
+    if (isRoot || isListItem) LogUtil.v('$initialIndent{',tag: tag);
+    data.keys.toList().asMap().forEach((index,key){
+      final isLast = index == data.length-1;
+      var value = data[key];
+      if(value is String) value = '\"$value\"';
+    });
+  }
+  static String _indent([int tabCount = 1]) => '  ' * tabCount;
 }
